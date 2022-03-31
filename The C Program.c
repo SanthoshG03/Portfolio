d3b24de96b413f340e9741836f089cb41e2700fe
SanthoshG03 <63532949+SanthoshG03@users.noreply.github.com>
2022-03-31 15:53:26 +0530

    Update The C Program.c

--- The C Program.c
+++ The C Program.c
@@ -2,10 +2,48 @@
 
 int main()
 {
-printf("The bytes value for int is: %ld  \n",sizeof(int));
-printf("By default signed integer is used & its bytes value are ranged from -32.768 to 32,767\n");
-  printf("\nThe bytes value for unsigned int is : %lu \n",sizeof(int));
-  printf("unsigned int is used to get only the positive values as result and \"(PERCENTAGE-lu)\" is used for it\n");
-  printf("\n The unsigned int bytes value are range from 0 to 65,535\n");
+printf(" The size of signed   int is             : %ld  bytes \n",sizeof(int));
+  
+  printf("\n The size of unsigned int is         : %lu bytes \n",sizeof(int));
+  
+  printf("\n The size of unsigned short int is   : %lu bytes \n", sizeof(short int));
+  
+  printf("\n The size of unsigend long int is    : %lu bytes \n", sizeof(long int));
+  /*
+   %lu - unsigned int 
+   %ld - signed int
+   %c  - char
+   %o  - octal
+   %s  - string
+   %x  - hexadecimal
+   %X  - also hexadecimal but Alphabetical char in UPPERCASE
+   %e or %E - Scientific Notation
+   %g  - decimal floating point number
+   %p  - prints the address in hexadecimal form
+   
+   %f  - floating point number 6 numbers after the dot(.)
+   %.2f - prints only TWO Decimals after the point value
+   
+   %8d - prints the number after the EIGHT White Spaces in the O/P
+   %-8d - prints the number left algined as it reduces the EIGHT White spaces before the O/P
+   
+   
+   lets say int =8
+   and %8d is used in printf() 
+   then output will be 00000008[SEVEN ZEROS AND 8]
+   
+  */
+  
+  printf("\n The size of unsigned char is        : %lu bytes \n", sizeof(char));
+  
+  //The char is always unsigned by default
+  
+  printf("\n The size of unsigned short is       : %lu bytes \n", sizeof(short));
+  
+  printf("\n The size of unsigned float is       : %lu bytes \n", sizeof(float));
+  
+  printf("\n The size of unsigned double is      : %lu bytes  \n", sizeof(double));
+  
+  printf("\n The size of unsigned long double is : %lu bytes \n", sizeof(long double));
   return 0;
   }
